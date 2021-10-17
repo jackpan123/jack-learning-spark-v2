e8af5ec914cae3d638e13bff5e78a0473116c65c
jackpan <826647196@qq.com>
2021-10-17 23:03:51 +0800

    Create CommonRelationalOperations.scala

---
+++ src/main/scala/com/jackpan/spark/chapter5/CommonRelationalOperations.scala
@@ -0,0 +1,52 @@
+package com.jackpan.spark.chapter5
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+/**
+ *
+ *
+ * @author jackpan
+ * @version v1.0 2021/10/17 22:51
+ */
+object CommonRelationalOperations {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder
+      .appName("CommonRelationalOperations")
+      .getOrCreate()
+
+    val delaysPath =
+      "data/departuredelays.csv"
+    val airportsPath =
+      "data/flights/airport-codes-na.txt"
+
+    val airports = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .option("delimiter", "\t")
+      .csv(airportsPath)
+
+    airports.createOrReplaceTempView("airports_na")
+
+    val delays = spark.read
+      .option("header", "true")
+      .csv(delaysPath)
+      .withColumn("delay", expr("CAST(delay as INT) as delay"))
+      .withColumn("distance", expr("CAST(distance as INT) as distance"))
+    delays.createOrReplaceTempView("departureDelays")
+
+    val foo = delays.filter(
+      expr(
+        """origin == 'SEA' AND destination == 'SFO' AND
+          date like '01010%' AND delay > 0"""))
+    foo.createOrReplaceTempView("foo")
+
+    spark.sql("SELECT * FROM airports_na LIMIT 10").show()
+
+    spark.sql("SELECT * FROM departureDelays LIMIT 10").show()
+
+    spark.sql("SELECT * FROM foo LIMIT 10").show()
+  }
+}
