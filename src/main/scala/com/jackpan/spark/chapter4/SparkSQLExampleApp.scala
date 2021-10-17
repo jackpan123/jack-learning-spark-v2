370e872e7f04f9f67f197588d705731338186521
jackpan <826647196@qq.com>
2021-10-17 11:35:16 +0800

    Add delay level

--- src/main/scala/com/jackpan/spark/chapter4/SparkSQLExampleApp.scala
+++ src/main/scala/com/jackpan/spark/chapter4/SparkSQLExampleApp.scala
@@ -29,6 +29,7 @@ object SparkSQLExampleApp {
     df.createOrReplaceTempView("us_delay_flights_tbl")
     spark.sql("""SELECT distance, origin, destination FROM us_delay_flights_tbl WHERE distance > 1000 ORDER BY distance DESC""")
       .show(10)
+    
 
     spark.sql(
       """SELECT date, delay, origin, destination FROM us_delay_flights_tbl WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD' ORDER BY delay DESC""")
@@ -39,6 +40,20 @@ object SparkSQLExampleApp {
     spark.sql(
       """SELECT MONTH(newDate) FROM us_delay_flights_table WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD' GROUP BY MONTH(newDate) ORDER BY MONTH(newDate) DESC""")
       .show(10)
+
+    spark.sql(
+      """SELECT delay, origin, destination,
+              CASE
+                  WHEN delay > 360 THEN 'Very Long Delays'
+                  WHEN delay >= 120 AND delay <= 360 THEN 'Long Delays'
+                  WHEN delay >= 60 AND delay < 120 THEN 'Short Delays'
+                  WHEN delay > 0 and delay < 60 THEN 'Tolerable Delays'
+                  WHEN delay = 0 THEN 'No Delays'
+                  ELSE 'Early'
+               END AS Flight_Delays
+               FROM us_delay_flights_tbl
+               ORDER BY origin, delay DESC
+         """).show(10)
 
 
   }
