3047cd6d8a83bce0cdece18732760c8b68a35588
jackpan <826647196@qq.com>
2021-10-19 13:09:09 +0800

    Lambda filter and map

--- src/main/scala/com/jackpan/spark/chapter6/SampleDatasetsExample.scala
+++ src/main/scala/com/jackpan/spark/chapter6/SampleDatasetsExample.scala
@@ -1,6 +1,7 @@
 package com.jackpan.spark.chapter6
 
 import scala.util.Random._
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -11,7 +12,7 @@ import org.apache.spark.sql.SparkSession
  */
 object SampleDatasetsExample {
 
-  case class Usage(uid:Int, uname:String, useage:Int)
+  case class Usage(uid:Int, uname:String, usage:Int)
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
@@ -26,5 +27,35 @@ object SampleDatasetsExample {
 
     val dsUsage = spark.createDataset(data)
     dsUsage.show(10)
+
+    dsUsage
+      .filter(d => d.usage > 900)
+      .orderBy(desc("usage"))
+      .show(5, false)
+
+    def filterWithUsage(u: Usage) = u.usage > 900
+
+    dsUsage
+      .filter(filterWithUsage(_))
+      .orderBy(desc("usage"))
+      .show(5, false)
+
+    dsUsage.map(u => {
+      if (u.usage > 750) {
+        u.usage * .15
+      } else {
+        u.usage * .50
+      }
+    }).show(5, false)
+
+    def computeCostUsage(usage: Int): Double = {
+      if (usage > 750) {
+        usage * .15
+      } else {
+        usage * .50
+      }
+    }
+
+    dsUsage.map(u => {computeCostUsage(u.usage)}).show(5, false)
   }
 }
