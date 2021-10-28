040d9e322a0aae394a06235b5c69692fa753270a
jackpan <826647196@qq.com>
2021-10-28 12:37:55 +0800

    Update ArbitraryStatefulComputationsExample.scala

--- src/main/scala/com/jackpan/spark/chapter8/ArbitraryStatefulComputationsExample.scala
+++ src/main/scala/com/jackpan/spark/chapter8/ArbitraryStatefulComputationsExample.scala
@@ -8,31 +8,39 @@ import org.apache.spark.sql.streaming._
  * @version v1.0 2021/10/27 13:28
  */
 object ArbitraryStatefulComputationsExample {
-  case class UserAction(userId: String, action: String)
+  case class UserAction(userId: String, action: Boolean)
 
   case class UserStatus(userId: String, active: Boolean)
   def updateUserStatus(userId: String,
                        newActions: Iterator[UserAction],
                        state: GroupState[UserStatus]): UserStatus = {
     val userStatus = state.getOption.getOrElse {
-      new UserStatus(userId, false)
+      UserStatus(userId, active = false)
     }
+    println(userStatus)
     newActions.foreach { action =>
-      userStatus.updateWith(action)
+      userStatus.copy(action.userId, action.action)
     }
-
+    println("After...")
+    println(userStatus)
     state.update(userStatus)
-    return userStatus;
+    userStatus
   }
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
       .appName("ArbitraryStatefulComputationsExample")
       .getOrCreate()
+
+//    val lines = spark
+//      .readStream.format("socket") .option("host", "localhost") .option("port", 9999).load()
     import spark.implicits._
-    val userActions: Dataset[UserAction] = {}
+    val userActions: Dataset[UserAction] = Seq(UserAction("1", action = false), UserAction("2", action = true), UserAction("3", action = false),
+      UserAction("4", action = true)).toDS()
     val latestStatuses = userActions
       .groupByKey(userAction => userAction.userId)
       .mapGroupsWithState(updateUserStatus _)
+
+    latestStatuses.show()
   }
 }
