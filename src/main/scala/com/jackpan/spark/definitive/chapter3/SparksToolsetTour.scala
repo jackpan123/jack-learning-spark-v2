e5b7f17352833099a1f72fc49bbc2a347125ae20
jackpan <826647196@qq.com>
2022-07-14 13:27:19 +0800

    Transfer Dataset

--- src/main/scala/com/jackpan/spark/definitive/chapter3/SparksToolsetTour.scala
+++ src/main/scala/com/jackpan/spark/definitive/chapter3/SparksToolsetTour.scala
@@ -2,6 +2,12 @@ package com.jackpan.spark.definitive.chapter3
 
 import org.apache.spark.sql.SparkSession
 
+
+
+case class Flight(DEST_COUNTRY_NAME: String,
+                  ORIGIN_COUNTRY_NAME: String,
+                  count: BigInt)
+
 object SparksToolsetTour {
 
   def main(args: Array[String]): Unit = {
@@ -11,12 +17,16 @@ object SparksToolsetTour {
       .getOrCreate()
 
     import spark.implicits._
-
-    case class Flight(DEST_COUNTRY_NAME: String,
-                      ORIGINAL_COUNTRY_NAME: String,
-                      count: BigInt)
-
     val flightsDF = spark.read.parquet("/Volumes/JackApp/jackproject/Spark-The-Definitive-Guide/data/flight-data/parquet/2010-summary.parquet/")
-    flightsDF.show()
+    val flights = flightsDF.as[Flight]
+
+    flights
+      .filter(flight_row => flight_row.ORIGIN_COUNTRY_NAME != "Canada")
+      .map(flight_row => flight_row)
+      .take(5)
+
+    flights.take(5)
+      .filter(flight_row => flight_row.ORIGIN_COUNTRY_NAME != "Canada")
+      .map(fr => Flight(fr.DEST_COUNTRY_NAME, fr.ORIGIN_COUNTRY_NAME, fr.count + 5))
   }
 }
