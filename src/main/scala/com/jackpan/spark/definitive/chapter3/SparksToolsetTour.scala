d81185ea7214382c7e4954ab0daddf02a60e729a
jackpan <826647196@qq.com>
2022-07-14 13:42:29 +0800

    Update SparksToolsetTour.scala

--- src/main/scala/com/jackpan/spark/definitive/chapter3/SparksToolsetTour.scala
+++ src/main/scala/com/jackpan/spark/definitive/chapter3/SparksToolsetTour.scala
@@ -17,8 +17,10 @@ object SparksToolsetTour {
       .getOrCreate()
 
     import spark.implicits._
-    val flightsDF = spark.read.parquet("/Volumes/JackApp/jackproject/Spark-The-Definitive-Guide/data/flight-data/parquet/2010-summary.parquet/")
+    val flightsDF = spark.read.parquet("/Users/jackpan/JackPanDocuments/jack-project/spark/jack-learning-spark-v2/data/flight_data/parquet/2010-summary.parquet")
+
     val flights = flightsDF.as[Flight]
+
 
     flights
       .filter(flight_row => flight_row.ORIGIN_COUNTRY_NAME != "Canada")
@@ -28,5 +30,13 @@ object SparksToolsetTour {
     flights.take(5)
       .filter(flight_row => flight_row.ORIGIN_COUNTRY_NAME != "Canada")
       .map(fr => Flight(fr.DEST_COUNTRY_NAME, fr.ORIGIN_COUNTRY_NAME, fr.count + 5))
+
+    val staticDataFrame = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("/Users/jackpan/JackPanDocuments/jack-project/spark/jack-learning-spark-v2/data/retail-data/by-day/*.csv")
+
+    staticDataFrame.createOrReplaceTempView("retail_data")
+    val staticSchema = staticDataFrame.schema
   }
 }
