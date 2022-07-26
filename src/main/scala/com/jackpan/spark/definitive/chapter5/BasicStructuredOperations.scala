aa6f075e908b7a4bbc02b58986696a266245b9ce
jackpan <826647196@qq.com>
2022-07-26 13:00:32 +0800

    Update BasicStructuredOperations.scala

--- src/main/scala/com/jackpan/spark/definitive/chapter5/BasicStructuredOperations.scala
+++ src/main/scala/com/jackpan/spark/definitive/chapter5/BasicStructuredOperations.scala
@@ -1,6 +1,8 @@
 package com.jackpan.spark.definitive.chapter5
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType}
+import org.apache.spark.sql.types.Metadata
 
 object BasicStructuredOperations {
   def main(args: Array[String]): Unit = {
@@ -13,5 +15,15 @@ object BasicStructuredOperations {
       .load("/Users/jackpan/JackPanDocuments/jack-project/spark/jack-learning-spark-v2/data/flight_data/json/2015-summary.json")
 
     df.printSchema()
+
+    val myManualSchema = StructType(Array(
+      StructField("DEST_COUNTRY_NAME", StringType, true),
+      StructField("ORIGIN_COUNTRY_NAME", StringType, true),
+      StructField("count", LongType, false, Metadata.fromJson("{\"hello\":\"world\"}"))
+    ))
+
+    val df1 = spark.read.format("json").schema(myManualSchema).
+      load("/Users/jackpan/JackPanDocuments/jack-project/spark/jack-learning-spark-v2/data/flight_data/json/2015-summary.json")
+    df1.printSchema()
   }
 }
