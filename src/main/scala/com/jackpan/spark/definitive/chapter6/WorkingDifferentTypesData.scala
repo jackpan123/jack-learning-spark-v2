0e0c7bf01eaf9eec6f5f3c16e9665ef2b1c73b08
jackpan <826647196@qq.com>
2022-08-01 21:08:38 +0800

    Working number

--- src/main/scala/com/jackpan/spark/definitive/chapter6/WorkingDifferentTypesData.scala
+++ src/main/scala/com/jackpan/spark/definitive/chapter6/WorkingDifferentTypesData.scala
@@ -59,6 +59,31 @@ object WorkingDifferentTypesData {
     // Use equals is null safe!
     df.where(col("Description").eqNullSafe("hello")).show()
 
+    val fabricatedQuantity = pow(col("Quantity") * col("UnitPrice"), 2) + 5
+    df.select(expr("CustomerId"), fabricatedQuantity.alias("realQuantity")).show(2)
 
+    df.selectExpr("CustomerId",
+    "(POWER((Quantity * UnitPrice), 2.0) + 5) as realQuantity").show(2)
+
+    df.select(round(col("UnitPrice"), 1).alias("rounded"), col("UnitPrice"))
+      .show(5)
+
+    df.select(round(lit("2.5")), bround(lit("2.5"))).show(2)
+    println(df.stat.corr("Quantity", "UnitPrice"))
+    df.select(corr("Quantity", "UnitPrice")).show()
+
+
+    df.describe().show()
+
+    val colName = "UnitPrice"
+    val quantileProbs = Array(0.5)
+    val relError = 0.05
+    println(df.stat.approxQuantile("UnitPrice", quantileProbs, relError).head)
+
+    df.stat.crosstab("StockCode", "Quantity").show()
+
+    df.stat.freqItems(Seq("StockCode", "Quantity")).show()
+
+    df.select(monotonically_increasing_id()).show(2)
   }
 }
